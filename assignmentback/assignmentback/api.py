5c36112edaff17549bc61ab0f1ba6c39d5f41c77
liber09 <linda.bergsangel@hotmail.com>
2023-02-02 05:56:47 +0100

    More api

--- assignmentback/assignmentback/api.py
+++ assignmentback/assignmentback/api.py
@@ -7,7 +7,20 @@ def root():
     with open("index.html") as f:
         return f.read()
 
+#Get person with id
 @app.get("/persons/{id}")
 def get_person_by_id(id: int):
     data = db.get(table="person", where=("id", str(id)))
+    return data
+
+#Get Restaurant with id
+@app.get("/restaurant/{id}")
+def get_restaurant_by_id(id: int):
+    data = db.get(table="restaurant", where=("id", str(id)))
+    return data
+
+#Get person with id
+@app.get("/reviuew/{id}")
+def get_review_by_id(id: int):
+    data = db.get(table="review", where=("id", str(id)))
     return data
