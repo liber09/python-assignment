23696996c2582fb3956c3af53b1349f29264cefe
liber09 <linda.bergsangel@hotmail.com>
2023-02-13 05:37:08 +0100

    setting up structure for django site

--- assignmentback/assignmentback/settings.py
+++ assignmentback/assignmentback/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #This is the application
+    'catalog.app.CatalogConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,8 @@ WSGI_APPLICATION = "assignmentback.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": BASE_DIR / "db.postgresql",
     }
 }
 
